f209009afcb247a3a49e70697aba7132d0540119
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-11-05 07:41:40 -0700

    add methods to create the autoencoder based emulator

--- build_models.py
+++ build_models.py
@@ -7,20 +7,6 @@ from tensorflow.keras import optimizers
 import tensorflow as tf
 
 
-def sampling(args):
-    """
-    Function that samples from the Gaussian distributions to the latent layer
-    :param args: mean and log of variance of the Gaussians
-    :return: sampled latent vector
-    """
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    # by default, random_normal has mean = 0 and std = 1.0
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
-
-
 def build_direct_emulator(layer_hps, signal_train, par_train, activation_func='relu'):
     """
     Function that builds the emulator.
@@ -44,22 +30,10 @@ def build_direct_emulator(layer_hps, signal_train, par_train, activation_func='r
     return emulator
 
 
-def build_ae_emulator(hps, layer_hps, signal_train, par_train, activation_func='relu'):
-    """
-    Function that builds the autoencoder and the emulator based onb the autoencoder described in Appendix A.
-    :param hps: hyperparameters, a dictionary with values for the latent layer dimensionality, beta, and gamma
-    :param layer_hps: the hyperparameters controlling the number of layers and their dimensionalities,
-    packed into nested lists
-    :param signal_train: numpy array of training signals
-    :param par_train: numpy array of training parameters
-    :param activation_func: str, name of a keras recognized activation function or a tf.keras.activations instance
-    (see https://keras.io/api/layers/activations/)
-    :return: the VAE and the emulator as keras model objects
-    """
+def build_autoencoder(layer_hps, activation_func='relu'):
     encoding_hidden_dims = layer_hps[0]  # the layers of the encoder
-    vae_input = Input(shape=(signal_train.shape[1],))  # input layer for VAE
-    # loop over the number of layers and build the encoder part of the VAE
-    # with layers of the given dimensionalities
+    ae_input = Input(shape=(signal_train.shape[1],))  # input layer for autoencoder
+    # loop over the number of layers and build the encoder with layers of the given dimensions
     for i, dim in enumerate(encoding_hidden_dims):
         if i == 0:
             input_layer = vae_input  # the first layer takes input from the input layer
@@ -68,15 +42,13 @@ def build_ae_emulator(hps, layer_hps, signal_train, par_train, activation_func='
         # using dense (fully connected) layers
         x = Dense(dim, activation=activation_func, name='encoder_hidden_layer_' + str(i))(input_layer)
     latent_dim = hps['latent_dim']  # dimensionality of latent layer
-    z_mean = Dense(latent_dim, name='z_mean')(x)  # mean values for the Gaussians
-    # in the latent layer
-    z_log_var = Dense(latent_dim, name='z_log_var')(x)  # log variance of Gaussians
-    # sample points from the Gaussians with given mean and variance
-    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+    z = Dense(latent_dim, name='z_mean')(x)  # vanilla autoencoder
+
+    # create the encoder
+    encoder = Model(vae_input, z)
 
     # now, the same procedure for the decoder as for the encoder
     decoding_hidden_dims = layer_hps[1]
-    decoder_layers = []  # list of decoder layers
     for i, dim in enumerate(decoding_hidden_dims):
         if i == 0:
             input_layer = z
@@ -89,29 +61,27 @@ def build_ae_emulator(hps, layer_hps, signal_train, par_train, activation_func='
     decoder_output_layer = Dense(signal_train.shape[1])  # output of decoder
     decoder_layers.append(decoder_output_layer)
     decoded = decoder_output_layer(x)
-    vae_output = decoded
+    ae_output = decoded
 
-    # create the VAE
-    vae = Model(vae_input, vae_output, name='VAE')
+    # create the AutoEncoder
+    autoencoder = Model(ae_input, ae_output, name='AutoEncoder')
 
-    # the VAE loss function
-    reconstruction_loss = mse(vae_input, vae_output)  # mean squared error
-    # get the unpreprocessed signal to get the amplitude
-    unproc_signal = vae_input + K.constant(np.mean(signal_train, axis=0) / np.std(signal_train))
-    amplitude = K.max(K.abs(unproc_signal), axis=-1)
-    reconstruction_loss /= K.square(amplitude)
-    # KL-part of the loss:
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    gamma = hps['gamma']
-    beta = hps['beta']
-    loss = (signal_train.shape[-1] * reconstruction_loss + beta * kl_loss) / (signal_train.shape[-1] * gamma)
-    vae.add_loss(loss)
+    # create the decoder
+    encoded_input = Input(shape=(latent_dim,))
+    for i, layer in enumerate(decoder_layers):
+        if i == 0:
+            input = encoded_input
+        else:
+            input = y
+        y = layer(input)
+    decoder = Model(encoded_input, y)
 
-    # make the emulator in the same way as the encoder
-    em_input_par = Input(shape=(par_train.shape[1],), name='em_input')
-    em_hidden_dims = layer_hps[2]
+    return autoencoder, encoder, decoder
+
+
+def build_ae_emulator(layer_hps):
+    em_input_par = Input(shape=(X_train.shape[1],), name='em_input')
+    em_hidden_dims = layer_hps
     for i, dim in enumerate(em_hidden_dims):
         if i == 0:
             input_layer = em_input_par
@@ -122,16 +92,4 @@ def build_ae_emulator(hps, layer_hps, signal_train, par_train, activation_func='
     # the latent layer of the emulator
     autoencoder_par = Dense(latent_dim, name='em_autoencoder')(x)
 
-    # the decoder layers are shared so we just get them from the list made above
-    for i, decoder_layer in enumerate(decoder_layers):
-        if i == 0:
-            output_par_in = autoencoder_par
-        else:
-            output_par_in = output_par
-        output_par = decoder_layer(output_par_in)
-
-    em_output = output_par
-    # define the emulator model
-    emulator = Model(em_input_par, em_output, name='emulator')
-
-    return vae, emulator
+    return emulator
