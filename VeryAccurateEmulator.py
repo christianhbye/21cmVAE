a66be5f1a020cf6b32c048cd565e6a85d2d6c1ee
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-11-08 13:59:56 -0500

    tweak LR scheduler params to optimize training, add comments

--- VeryAccurateEmulator.py
+++ VeryAccurateEmulator.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
 import tensorflow as tf
+
 import build_models as bm
 import preprocess as pp
 from training_tools import train_emulator, train_ae_emulator, em_loss_fcn
@@ -10,7 +11,7 @@ class VeryAccurateEmulator:
     def __init__(self, **kwargs):
         """
         :param emulator: Keras model object, sets the default direct emulator if you have one
-        :return None
+        :return: None
         """
         # initialize training set, validation set, and test set variables
         with h5py.File('dataset_21cmVAE.h5', 'r') as hf:
@@ -31,32 +32,34 @@ class VeryAccurateEmulator:
         self.epochs = 350  # max number of epochs (can be less due to early stopping)
         self.learning_rate = 0.01  # initial learning rate for emulator
 
-        # Parameters that control the learning rate schedule during training:
-        self.lr_factor = 0.9
-        self.lr_patience = 5
-        self.min_lr = 1e-6
-        # if the loss doesn't by more than this, LR is reduced:
-        self.lr_min_delta = 1e-8
-        # for early stopping
-        self.es_patience = 15  # number of epochs to wait before stopping training
-        # if the loss doesn't decrease to less than this factor, training is stopped:
-        self.es_min_delta = 5e-10
+        # Parameters that control the learning rate schedule during training (see https://keras.io/api/callbacks/reduce_lr_on_plateau/):
+        # if the loss doesn't by more than [lr_min_delta] for [lr_patience] number of epochs, LR is multiplied by [lr_factor]
+        self.lr_min_delta = 5e-9
+        self.lr_factor = 0.95
+        self.lr_patience = 5  
+        self.min_lr = 1e-4  # minimum allowed lr
+        # similar parameters for for early stopping (https://keras.io/api/callbacks/early_stopping/)
+        self.es_patience = 15
+        self.es_min_delta = 1e-10
 
         for key, values in kwargs.items():
             if key not in set(['emulator']):
                 raise KeyError("Unexpected keyword argument passed to class VeryAccurateEmulator")
+        
+        # the default emulator is 21cmVAE:
         self.emulator = kwargs.pop('emulator', tf.keras.models.load_model('models/emulator.h5',
                                                                           custom_objects={'em_loss_fcn': em_loss_fcn}))
+        
         # initialize lists with losses, these get updated when models are trained
-        self.train_losses = []  # training set losses for direct emulator
-        self.val_losses = []  # validation set losses for direct emulator
+        self.train_losses = []  # training set losses
+        self.val_losses = []  # validation set losses
 
         # sampling redshifts: it is possible to train with signals that are not sampled with the same resolution
-        # or across the same redshift/frequency range. IN that case, these properties should be updated.
+        # or across the same redshift/frequency range. In that case, these properties should be updated.
         self.z_sampling = np.arange(5, 50 + 0.1, 0.1)  # redshifts
 
         def z_to_nu(redshift):
-            rest_frequency = 1420405751.7667  # rest frequency in Hz
+            rest_frequency = 1420405751.7667  # rest frequency in Hz of the 21-cm transition
             freqs = rest_frequency / (1 + redshift)
             freqs /= 1e6  # convert to MHz
             return freqs
@@ -67,19 +70,17 @@ class VeryAccurateEmulator:
         """
         Set the hyperparameters of the model.
         Possible **kwargs are:
-        direct_em_dims: list of ints, dimensions of each direct emulator layer, e.g [288, 352, 288, 224]
+        hidden_dims: list of ints, dimensions of each direct emulator layer, e.g [288, 352, 288, 224]
         activation_function: str, name of a keras recognized activation function or a tf.keras.activations instance
         (see https://keras.io/api/layers/activations/). Used in all hidden layers.
         :return: None
         """
         for key, values in kwargs.items():
-            if key not in set(['direct_em_dims', 'activation_func']):
+            if key not in set(['hidden_dims', 'activation_func']):
                 raise KeyError("Unexpected keyword argument in set_hyperparameters()")
 
         self.hidden_dims = kwargs.pop('hidden_dims', self.hidden_dims)
         self.activation_func = kwargs.pop('activation_function', self.activation_func)
-
-        return None
 
     def get_hyperparameters(self):
         """
@@ -92,22 +93,23 @@ class VeryAccurateEmulator:
 
     def train(self, **kwargs):
         """
-        Builds and trains the direct emulator without VAE. Possible kwargs are
+        Builds and trains the emulator. Possible kwargs are
         signal_train: numpy array of training signals
         par_train: numpy array of training set parameters
         signal_val: numpy array of validation signals
         par_val: numpy array of validation set parameters
-        em_lr: float, initial learning rate of emulator
+        learning_rate: float, initial learning rate of emulator
         epochs: int, number of epochs to train for
-        em_lr_factor: float, factor * old LR (learning rate) is the new LR for the emulator (used for LR schedule)
-        em_lr_patience: float, max number of epochs loss has not decreased for the emulator before reducing LR
+        lr_factor: float, factor * old LR (learning rate) is the new LR for the emulator (used for LR schedule)
+        lr_patience: float, max number of epochs loss has not decreased for the emulator before reducing LR
         (used for LR schedule)
-        em_min_lr: float, minimum allowed learning rate for emulator
-        lr_max_factor: float, max_factor * current loss is the max acceptable loss, a larger loss means that the counter
-        is added to, when it reaches the 'patience', the LR is reduced (used for LR schedule)
+        min_lr: float, minimum allowed learning rate for emulator
+        lr_min_delta: float, min delta in validation loss before LR is reduced
         es_patience: float, max number of epochs loss has not decreased before early stopping
-        es_max_factor: float, max_factor * current loss is the max acceptable loss, a larger loss for either the VAE or
-        the emulator means that the counter is added to, when it reaches the 'patience', early stopping is applied
+        es_min_delta: float, min delta in validation loss before early stoppping is applied
+        
+        Note: For more information about the LR schedule and Early stopping callbacks, please see:
+        https://keras.io/api/callbacks/reduce_lr_on_plateau/ and https://keras.io/api/callbacks/early_stopping/
 
         :return: None
         """
@@ -136,7 +138,8 @@ class VeryAccurateEmulator:
 
         # update the default models
         self.emulator = emulator
-
+        
+        # train the emulator
         losses = train_emulator(emulator, self.signal_train, self.signal_val, self.par_train, self.par_val, self.epochs,
                                 self.learning_rate, self.lr_factor, self.lr_patience, self.lr_min_delta, self.min_lr,
                                 self.es_min_delta, self.es_patience)
@@ -237,20 +240,61 @@ class VeryAccurateEmulator:
 class AutoEncoderEmulator:
     def __init__(self, **kwargs):
         """
-        :param autoencoder: Keras model object, sets the default VAE if you already have a trained one
-        :param encoder:
-        :param decoder:  make sure it matches the autoencoder
-        :param emulator: Keras model object, sets the default emulator if you have one
+        :param autoencoder: Keras model object, sets the default Autoencoder if you already have a trained one. If so, then also an encoder
+        and decoder must be set!
+        :param encoder: Keras model object, sets the default Encoder if you have one. Must be provided if decoder is provided.
+        :param decoder:  Keras model object, sets the default Decoder if you have one. Must be provided if encoder is provided.
+        :param emulator: Keras model object, sets the default emulator if you have one.
         These models can be loaded from file with tf.keras.models.load_model(). If None, then the trained models
         giving the best performance in Bye et al. 2021 will be default (the h5 files are in the 'models' directory).
         Please note: the models *must* be Keras model objects (or None) for things to work properly if they're used.
         The default models will be updated to the most recently trained models with the train() method.
+        
+        A note about the Autoencoder, Encoder and Decoder kwargs: By definition, Decoder(Encoder(x)) == Autoencoder(x) - if you use custom
+        models you must ensure this is the case. An AssertionError will be raised otherwise.
+        Moreover, this property requires the following combination of input parameters:
+        (1) No autoencoder, no encoder, no decoder
+        (2) No autoencoder, but encoder and decoder. In this case, the auteoncoder will be defined as Auteoncder = Decoder(Encoder)
+        (3) All three. In this case they need to satisfy Autoencoder = Decoder(Encoder)
+        If another combination of input parameters are given, they will have no effect (kwargs will be reset to default).
+        
+        :return: None
         """
         for key, values in kwargs.items():
             if key not in set(
                     ['autoencoder', 'encoder', 'decoder', 'emulator']):
                 raise KeyError("Unexpected keyword argument in class AutoEncoderEmulator")
-
+                
+        # checking for input combinations (see docstring):
+        if not 'encoder' in kwargs:
+            if 'autoencoder' in kwargs:
+                print('Kwarg autoencoder without encoder has no effect. Resetting to default autoencoder.')
+                kwargs.pop('autoencoder')
+            if 'decoder' in kwargs:
+                print('Kwarg decoder without encoder has no effect. Resetting to default decoder.')
+                kwargs.pop('decoder')
+        elif not 'decoder' in kwargs:
+            print('Kwarg encoder without decoder has no effect. Resetting to default encoder.')
+            kwargs.pop('encoder')
+            if 'autoencoder' in kwargs:
+                print('Kwarg autoencoder without decoder has no effect. Resetting to default autoencoder.')
+                kwarsg.pop('autoencoder')
+        else:  # both encoder and decoder are present
+            en, de = kwargs['encoder'], kwargs['decoder']
+            ae = tf.keras.models.Model(inputs=en.input, outputs=de(en.output))
+            if not 'autoencoder' in kwargs:
+                print('Creating Autoencoder from Encoder and Decoder')
+                kwargs['autoencoder'] = ae
+            else:
+                assert ae == kwargs['autoencoder'], 'The Autoencoder does not match the Encoder and Decoder!'
+        
+        # the default models are the pretrained ones
+        model_path = 'models/autoencoder_based_emulator/'
+        self.autoencoder = kwargs.pop('autoencoder', tf.keras.models.load_model(model_path+'autoencoder.h5'))
+        self.encoder = kwargs.pop('encoder', tf.keras.models.load_model(model_path+'encoder.h5'))
+        self.decoder = kwargs.pop('decoder', tf.keras.models.load_model(model_path+'decoder.h5'))
+        self.emulator = kwargs.pop('emulator', tf.keras.models.load_model(model_path+'ae_emulator.h5',
+                                                                          custom_objects={'em_loss_fcn': em_loss_fcn}))
         # initialize training set, validation set, and test set variables
         with h5py.File('dataset_21cmVAE.h5', 'r') as hf:
             self.signal_train = hf['signal_train'][:]
@@ -271,32 +315,24 @@ class AutoEncoderEmulator:
 
         # initialize parameters for training
         self.epochs = 350  # max number of epochs (can be less due to early stopping)
-        self.ae_lr = 0.001  # initial learning rate for VAE
+        self.ae_lr = 0.01  # initial learning rate for VAE
         self.em_lr = 0.01  # initial learning rate for emulator
 
         # Parameters that control the learning rate schedule during training:
-        self.ae_lr_factor = 0.75  # factor LR is multiplied with when reduced
+        self.ae_lr_factor = 0.9  # factor LR is multiplied with when reduced
         self.ae_lr_patience = 5  # number of epochs to wait before reducing LR
-        self.ae_min_lr = 1e-6  # minimum allowed LR
-        self.em_lr_factor = 0.85
+        self.ae_min_lr = 1e-4  # minimum allowed LR
+        self.em_lr_factor = 0.9
         self.em_lr_patience = 5
-        self.em_min_lr = 1e-6
+        self.em_min_lr = 1e-4
         # if the loss doesn't decrease by more than this, LR is reduced:
-        self.ae_lr_min_delta = 1e-5
-        self.em_lr_min_delta = 1e-4
+        self.ae_lr_min_delta = 5e-9
+        self.em_lr_min_delta = 5e-3
         # for early stopping
         self.es_patience = 15  # number of epochs to wait before stopping training
         # if the loss doesn't decrease by more than this, training is stopped:
-        self.ae_earlystop_delta = 5e-6
-        self.em_earlystop_delta = 1e-4
-
-        # the default models are the pretrained ones
-        model_path = 'models/autoencoder_based_emulator/'
-        self.autoencoder = kwargs.pop('autoencoder', tf.keras.models.load_model(model_path+'autoencoder.h5'))
-        self.encoder = kwargs.pop('encoder', tf.keras.models.load_model(model_path+'encoder.h5'))
-        self.decoder = kwargs.pop('decoder', tf.keras.models.load_model(model_path+'decoder.h5'))
-        self.emulator = kwargs.pop('emulator', tf.keras.models.load_model(model_path+'ae_emulator.h5',
-                                                                          custom_objects={'em_loss_fcn': em_loss_fcn}))
+        self.ae_earlystop_delta = 5e-10
+        self.em_earlystop_delta = 5e-5
 
         # initialize lists with losses, these get updated when models are trained
         self.ae_train_losses = []  # training set losses for VAE
@@ -339,8 +375,6 @@ class AutoEncoderEmulator:
         self.em_dims = kwargs.pop('em_dims', self.em_dims)
         self.activation_func = kwargs.pop('activation_function', self.activation_func)
 
-        return None
-
     def get_hyperparameters(self):
         """
         Method that prints the current hyperparameters. Takes no input arguments.
@@ -360,30 +394,33 @@ class AutoEncoderEmulator:
         par_train: numpy array of training set parameters
         signal_val: numpy array of validation signals
         par_val: numpy array of validation set parameters
-        vae_lr: float, initial learning rate of VAE
+        ae_lr: float, initial learning rate of autoencoder
         em_lr: float, initial learning rate of emulator
         epochs: int, number of epochs to train for
-        vae_lr_factor: float, factor * old LR (learning rate) is the new LR for the VAE (used for LR schedule)
-        vae_lr_patience: float, max number of epochs loss has not decreased for the VAE before reducing LR (used for
+        ae_lr_factor: float, factor * old LR (learning rate) is the new LR for the autoencoder (used for LR schedule)
+        ae_lr_patience: float, max number of epochs loss has not decreased for the autoencoder before reducing LR (used for
         LR schedule)
-        vae_min_lr: float, minimum allowed learning rate for VAE
+        ae_min_lr: float, minimum allowed learning rate for autoencoder
+        ae_min_delta: float, min decrease in autoencoder validation loss in LR scheduler
+        ae_earlystop_delta: same as min delta but for the early stop callback
         em_lr_factor: float, factor * old LR (learning rate) is the new LR for the emulator (used for LR schedule)
         em_lr_patience: float, max number of epochs loss has not decreased for the emulator before reducing LR
         (used for LR schedule)
         em_min_lr: float, minimum allowed learning rate for emulator
-        lr_max_factor: float, max_factor * current loss is the max acceptable loss, a larger loss means that the counter
-        is added to, when it reaches the 'patience', the LR is reduced (used for LR schedule)
+        em_min_delta: float, min decrease in emulator validation loss in LR scheduler
+        em_earlystop_delta: same as min delta but for the early stop callback
         es_patience: float, max number of epochs loss has not decreased before early stopping
-        es_max_factor: float, max_factor * current loss is the max acceptable loss, a larger loss for either the VAE or
-        the emulator means that the counter is added to, when it reaches the 'patience', early stopping is applied
-
+        
+        Note: For more information about the LR schedule and Early stopping callbacks, please see:
+        https://keras.io/api/callbacks/reduce_lr_on_plateau/ and https://keras.io/api/callbacks/early_stopping/
+        
         :return: None
         """
         for key, values in kwargs.items():
             if key not in set(
-                    ['signal_train', 'par_train', 'signal_val', 'par_val', 'vae_lr', 'em_lr', 'epochs',
-                     'vae_lr_factor', 'vae_lr_patience', 'vae_min_lr', 'em_lr_factor', 'em_lr_patience',
-                     'em_min_lr', 'lr_max_factor', 'es_patience', 'es_max_factor']):
+                    ['signal_train', 'par_train', 'signal_val', 'par_val', 'ae_lr', 'em_lr', 'epochs', 'ae_lr_factor', 'ae_lr_patience',
+                     'ae_min_lr', 'ae_min_delta', 'ae_earlystop_delta', 'em_lr_factor', 'em_lr_patience', 'em_min_lr', 'em_min_delta',
+                     'es_patience', 'em_earlystop_delta']):
                 raise KeyError("Unexpected keyword argument in train()")
 
         # update the properties
@@ -420,8 +457,8 @@ class AutoEncoderEmulator:
         self.emulator = emulator
 
         losses = train_ae_emulator(self.autoencoder, self.encoder, self.emulator, self.signal_train, self.signal_val,
-                                   self.par_train, self.par_val, self.epochs, self.ae_lr, self.em_lr, self.ae_lr_factor, self.ae_lr_patience,
-                                   self.ae_lr_min_delta, self.ae_min_lr, self.ae_earlystop_delta, self.es_patience,
+                                   self.par_train, self.par_val, self.epochs, self.ae_lr, self.em_lr, self.ae_lr_factor,
+                                   self.ae_lr_patience, self.ae_lr_min_delta, self.ae_min_lr, self.ae_earlystop_delta, self.es_patience,
                                    self.em_lr_factor, self.em_lr_patience, self.em_lr_min_delta, self.em_min_lr,
                                    self.em_earlystop_delta, self.es_patience)
 
