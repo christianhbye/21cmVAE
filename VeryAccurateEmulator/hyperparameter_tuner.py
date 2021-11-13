bd365cab6c343557ea50cdd356fc3c2e3da9d98d
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-11-13 00:17:31 -0800

    fix some bugs and typos

--- VeryAccurateEmulator/hyperparameter_tuner.py
+++ VeryAccurateEmulator/hyperparameter_tuner.py
@@ -13,10 +13,12 @@ from build_models import build_direct_emulator
 from training_tools import train_emulator
 
 
-MAX_TRIALS = 500 # number of models to build
-EPOCHS = 350 # max epochs for training (we use early stopping as well)
+MAX_TRIALS = 500  # number of models to build
+EPOCHS = 350  # max epochs for training (we use early stopping as well)
 
-with h5py.File('dataset_21cmVAE.h5', 'r') as hf:
+SCRIPT_PATH = os.path.realpath(__file__)[:-len('VeryAccurateEmulator.py')]
+
+with h5py.File(SCRIPT_PATH + 'dataset_21cmVAE.h5', 'r') as hf:
     signal_train = hf['signal_train'][:]
     signal_val = hf['signal_val'][:]
     signal_test = hf['signal_test'][:]
@@ -46,10 +48,10 @@ em_lr = 0.01
 em_lr_factor = 0.95
 em_lr_patience = 5
 em_min_lr = 1e-4
-lr_min_delta = 5e-9
+em_lr_min_delta = 5e-9
 
 # for early stopping (https://keras.io/api/callbacks/early_stopping/)
-early_stop_patience = 15
+es_patience = 15
 es_min_delta = 1e-10
 
 
@@ -77,11 +79,11 @@ def generate_layer_hps(no_hidden_layers=(min_hidden_layers, h_layer_step, max_st
     #generate hyperparams for layers
     number_of_layers = generate_hp(*no_hidden_layers)
     # initialize empty array
-    hidden_dims = np.empty(number_of_layers)
+    hidden_dim_arr = np.empty(number_of_layers)
     for i in range(number_of_layers):
-        dim = generate_hp(*hidden_dims) # dimensionality of each layer
-        hidden_dims[i] = dim
-    return hidden_dims
+        dim = generate_hp(*hidden_dims)  # dimensionality of each layer
+        hidden_dim_arr[i] = dim
+    return hidden_dim_arr
 
 # Input variables
 X_train = pp.par_transform(par_train, par_train)
@@ -104,9 +106,9 @@ with open('search_range.txt', 'w') as f:
     f.write('\nLearning rate = {}'.format(em_lr))
     f.write('\nlr_factor = {}'.format(em_lr_factor))
     f.write('\nlr_patience = {}'.format(em_lr_patience))
-    f.write('\nLearning rate min delta = {}'.format(lr_min_delta))
+    f.write('\nLearning rate min delta = {}'.format(em_lr_min_delta))
     f.write('\nmin_lr = {}'.format(em_min_lr))
-    f.write('\nearly_stop_patience = {}'.format(early_stop_patience+))
+    f.write('\nearly_stop_patience = {}'.format(es_patience))
     f.write('\nEarly stop min delta = {}'.format(es_min_delta))
 
 
@@ -130,6 +132,7 @@ def save_results(trial, layer_hps, emulator, losses):
     # save hps
     np.save(fname+'_layer_hps.npy', layer_hps)
 
+
 def delete_results(trial):
     """
     Function that delete results of trial. This gets called when the trial is no longer among the five best.
@@ -139,6 +142,7 @@ def delete_results(trial):
     fname = 'results_trial_' + str(int(trial))
     os.remove(fname+'.txt')
     os.remove(fname+'_layer_hps.npy')
+
 
 def run_tuner(max_trials=MAX_TRIALS):
     """
@@ -156,15 +160,16 @@ def run_tuner(max_trials=MAX_TRIALS):
         losses = train_emulator(emulator, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr, em_lr_factor, em_lr_patience,
                                 em_lr_min_delta, em_min_lr, es_min_delta, es_patience)
         # we care about the epoch that gives the minimum validation loss for the emulator ...
-        em_loss_val = losses[-1]
-        min_indx = np.argmin(em_loss_val)  # ... this is that epoch
+        validation_loss = losses[-1]
+        min_indx = np.argmin(validation_loss)  # ... this is that epoch
         min_losses = [loss[min_indx] for loss in losses]  # make a list of the values of the losses for that epoch
         em_loss_val_min = min_losses[-1]
         if i < 5:  # if this is among the first five trials, we save the results anyway
             five_best_val[i, 0] = i
             five_best_val[i, 1] = em_loss_val_min
             save_results(i, layer_hps, emulator, min_losses)
-        elif em_loss_val_min < np.max(five_best_val[:, 1]):  # for subsequent trials, we save the results if they're better than previous
+        # for subsequent trials, we save the results if they're better than previous
+        elif em_loss_val_min < np.max(five_best_val[:, 1]):
             idx = np.argmax(five_best_val[:, 1])
             worst_trial = five_best_val[idx, 0]
             delete_results(worst_trial)
@@ -176,20 +181,19 @@ def run_tuner(max_trials=MAX_TRIALS):
         K.clear_session()
     return five_best_val
 
+
 five_best_trials = run_tuner()
+
 
 # the very best trial:
 best_trial_idx = np.argmin(five_best_trials[:, 1])
 best_trial = int(five_best_trials[best_trial_idx, 0])
 # load the hyperparameters of that trial
-best_layer_hps = []
-best_layer_hps_list = np.load('results_trial_'+str(best_trial)+'_layer_hps.npy', allow_pickle=True)
-for i in range(len(best_layer_hps_list)):
-    best_layer_hps.append(best_layer_hps_list[i])
+best_layer_hps = np.load('results_trial_'+str(best_trial)+'_layer_hps.npy', allow_pickle=True)
 
 # now, retrain the emulator and VAE with the best hyperparameters
 tuned_em = build_direct_emulator(best_layer_hps, signal_train, par_train)
-em_loss, em_loss_val = train_emulator(tuned_emulator, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr, em_lr_factor,
+em_loss, em_loss_val = train_emulator(tuned_em, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr, em_lr_factor,
                                       em_lr_patience, em_lr_min_delta, em_min_lr, es_min_delta, es_patience)
 
 # Can plot the  training curves like this
