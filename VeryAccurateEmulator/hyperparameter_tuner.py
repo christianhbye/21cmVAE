e30e6f41faa7fd5b4f772f2f815a87a3847b7477
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-11-14 17:45:02 -0800

    start cleaning up hp tuner

--- VeryAccurateEmulator/hyperparameter_tuner.py
+++ VeryAccurateEmulator/hyperparameter_tuner.py
@@ -13,51 +13,60 @@ from VeryAccurateEmulator.build_models import build_direct_emulator
 from VeryAccurateEmulator.training_tools import train_emulator
 
 
-MAX_TRIALS = 500  # number of models to build
-EPOCHS = 350  # max epochs for training (we use early stopping as well)
+class HyperParameterTuner:
+    def __init__(self, max_trials=500, epochs=350, min_hidden_layers=1, max_step_h_layers=4, h_layer_step=1,
+                 min_hidden_dim = 32, max_step_hidden_dim = 6, hidden_dim_step=64):
+        self.max_trials = max_trials  # number of models to build
+        self.epochs = epochs  # max epochs for training (we use early stopping as well)
 
-SCRIPT_PATH = os.path.realpath(__file__)[:-len('VeryAccurateEmulator.py')]
+        script_path = os.path.realpath(__file__)[:-len('VeryAccurateEmulator.py')]
+        with h5py.File(script_path + 'dataset_21cmVAE.h5', 'r') as hf:
+            self.signal_train = hf['signal_train'][:]
+            self.signal_val = hf['signal_val'][:]
+            self.signal_test = hf['signal_test'][:]
+            self.par_train = hf['par_train'][:]
+            self.par_val = hf['par_val'][:]
+            self.par_test = hf['par_test'][:]
+        self.par_labels = ['fstar', 'Vc', 'fx', 'tau', 'alpha', 'nu_min', 'Rmfp']
 
-with h5py.File(SCRIPT_PATH + 'dataset_21cmVAE.h5', 'r') as hf:
-    signal_train = hf['signal_train'][:]
-    signal_val = hf['signal_val'][:]
-    signal_test = hf['signal_test'][:]
-    par_train = hf['par_train'][:]
-    par_val = hf['par_val'][:]
-    par_test = hf['par_test'][:]
+        # (fixed) hyperparameters that define the search range
+        self.min_hidden_layers = min_hidden_layers  # min. no. of hidden layers
+        self.max_step_h_layers = max_step_h_layers  # max. no. of hidden layers
+        self.h_layer_step = h_layer_step
+        self.min_hidden_dim = min_hidden_dim  # min. dimensionality of hidden layer
+        self.max_step_hidden_dim = max_step_hidden_dim  # max. dimensionality of hidden layer
+        self.hidden_dim_step = hidden_dim_step  # step size
 
-par_labels = ['fstar', 'Vc', 'fx', 'tau', 'alpha', 'nu_min', 'Rmfp']
+        assert type(self.min_hidden_layers) == int
+        assert type(self.max_step_h_layers) == int
+        assert type(self.h_layer_step) == int
+        assert type(self.min_hidden_dim) == int
+        assert type(self.max_step_hidden_dim) == int
+        assert type(self.hidden_dim_step) == int
+
+        # fixed hyperparameters
+        self.activation_func = 'relu'
+        self.em_lr = 0.01
+        assert type(self.em_lr) == float or int
+
+        # Reduce LR callback (https://keras.io/api/callbacks/reduce_lr_on_plateau/)
+        self.em_lr_factor = 0.95
+        self.em_lr_patience = 5
+        self.em_min_lr = 1e-4
+        self.em_lr_min_delta = 5e-9
+        assert type(self.em_lr_factor) == float or int
+        assert type(self.em_lr_patience) == int
+        assert type(self.em_min_lr) == float or int
+        assert type(self.em_lr_min_delta) == flaot or int
+        assert self.em_min_lr <= self.em_lr, "Min LR must be <= initial LR"
 
 signal_train_preproc = pp.preproc(signal_train, signal_train)
 signal_val_preproc = pp.preproc(signal_val, signal_train)
 signal_test_preproc = pp.preproc(signal_test, signal_train)
 
-# (fixed) hyperparameters that define the search range
-min_hidden_layers = 1  # min. no. of hidden layers
-max_step_h_layers = 4  # max. no. of hidden layers
-h_layer_step = 1
-min_hidden_dim = 32  # min. dimensionality of hidden layer
-max_step_hidden_dim = 6  # max. dimensionality of hidden layer
-hidden_dim_step = 64  # step size
 
-assert all(isinstance(var, int) for var in [min_hidden_layers, max_step_h_layers, h_layer_step, min_hidden_dim,
-                                            max_step_hidden_dim, hidden_dim_step])
 
-# fixed hyperparameters
-activation_func = 'relu'
-em_lr = 0.01
-assert type(em_lr) == float or int
 
-# Reduce LR callback (https://keras.io/api/callbacks/reduce_lr_on_plateau/)
-em_lr_factor = 0.95
-em_lr_patience = 5
-em_min_lr = 1e-4
-em_lr_min_delta = 5e-9
-assert type(em_lr_factor) == float or int
-assert type(em_lr_patience) == int
-assert type(em_min_lr) == float or int
-assert type(em_lr_min_delta) == flaot or int
-assert em_min_lr <= em_lr, "Min LR must be <= initial LR"
 
 # for early stopping (https://keras.io/api/callbacks/early_stopping/)
 es_patience = 15
@@ -66,13 +75,13 @@ assert type(es_patience) == int
 assert type(es_min_delta) == float or int
 
 
-def generate_hp(min_val, step_size, max_step):
+def generate_hp(min_val: int, step_size: int, max_step: int) -> int:
     """
     Function that generates one hyperparameter given the search range
-    :param min_val: float, min value of the hp
-    :param step_size: float or int, resolution of grid
+    :param min_val: int, min value of the hp
+    :param step_size: int, resolution of grid
     :param max_step: int, max multiple of step_size that can be added to min
-    :return: float, the hyperparameter
+    :return: int, the hyperparameter
     """
     step = np.random.randint(0, max_step+1)
     return min_val + step * step_size
@@ -81,12 +90,17 @@ def generate_hp(min_val, step_size, max_step):
 def generate_layer_hps(no_hidden_layers=(min_hidden_layers, h_layer_step, max_step_h_layers),
                        hidden_dims=(min_hidden_dim, hidden_dim_step, max_step_hidden_dim)):
     """
-    Get the hyperparameters that control the architecture, i.e. the number of layers and their dimensions. Input must be tuples of the form
-    (min, step size, max number of steps).
+    Get the hyperparameters that control the architecture, i.e. the number of layers and their dimensions.
+    Input must be tuples of the form (min, step size, max number of steps).
     :param no_hidden_layers: Number of emulator layers
     :param hidden_dims: Dimensionality of the layers
     :return: tuple of lists, giving the dimensionality of each layer for the encoder, decoder, and emulator
     """
+    assert len(no_hidden_layers) == 3
+    assert len(hidden_dims) == 3
+    assert all(isinstance(h, int) for h in no_hidden_layers)
+    assert all(isinstance(h, int) for h in hidden_dims)
+    assert min_hidden_dim > 0
     #generate hyperparams for layers
     number_of_layers = generate_hp(*no_hidden_layers)
     # initialize empty array
@@ -104,9 +118,10 @@ X_val = pp.par_transform(par_val, par_train)
 y_train = pp.preproc(signal_train, signal_train)
 y_val = pp.preproc(signal_val, signal_train)
 
-
+TIME = time.time() # to be saved in files to make them identifiable between different runs
 # save search range:
-with open('search_range.txt', 'w') as f:
+with open(sr_fname, 'a') as f:
+    f.write('\n ---------- \n HYPERPARAMETER RUN, time = {:.0f}'.format(TIME))
     f.write('\nmin_hidden_layers = {}'.format(min_hidden_layers))
     f.write('\nmax_step_h_layers = {}'.format(max_step_h_layers))
     f.write('\nh_layer_step = {}'.format(h_layer_step))
@@ -134,14 +149,15 @@ def save_results(trial, layer_hps, emulator, losses):
     :return: None
     """
     fname = 'results_trial_' + str(trial)
-    with open(fname+'.txt', 'w') as f:
+    with open(fname+'.txt', 'a') as f:
+        f.write('\n ---------- \n HYPERPARAMETER RUN, time = {:.0f}'.format(TIME))
         f.write('\n\nEmulator Losses:')
         f.write('\nTrain = {:.4f}'.format(losses[0]))
         f.write('\nValidation = {:.4f}'.format(losses[1]))
         f.write('\n')
         emulator.summary(print_fn=lambda x: f.write(x + '\n'))
     # save hps
-    np.save(fname+'_layer_hps.npy', layer_hps)
+    np.save(fname+'_{:.0f}_layer_hps.npy'.format(TIME), layer_hps)
 
 
 def delete_results(trial):
@@ -152,7 +168,7 @@ def delete_results(trial):
     """
     fname = 'results_trial_' + str(int(trial))
     os.remove(fname+'.txt')
-    os.remove(fname+'_layer_hps.npy')
+    os.remove(fname+'_{:.0f}_layer_hps.npy'.format(TIME))
 
 
 def run_tuner(max_trials=MAX_TRIALS):
@@ -168,8 +184,8 @@ def run_tuner(max_trials=MAX_TRIALS):
         # build the NN
         emulator = build_direct_emulator(layer_hps, signal_train, par_train)
         # train and get the losses
-        losses = train_emulator(emulator, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr, em_lr_factor, em_lr_patience,
-                                em_lr_min_delta, em_min_lr, es_min_delta, es_patience)
+        losses = train_emulator(emulator, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr, em_lr_factor,
+                                em_lr_patience, em_lr_min_delta, em_min_lr, es_min_delta, es_patience)
         # we care about the epoch that gives the minimum validation loss for the emulator ...
         validation_loss = losses[-1]
         min_indx = np.argmin(validation_loss)  # ... this is that epoch
@@ -200,12 +216,13 @@ five_best_trials = run_tuner()
 best_trial_idx = np.argmin(five_best_trials[:, 1])
 best_trial = int(five_best_trials[best_trial_idx, 0])
 # load the hyperparameters of that trial
-best_layer_hps = np.load('results_trial_'+str(best_trial)+'_layer_hps.npy', allow_pickle=True)
+best_layer_hps = np.load('results_trial_'+str(best_trial)+'_{:.0f}_layer_hps.npy'.format(TIME), allow_pickle=True)
 
 # now, retrain the emulator and VAE with the best hyperparameters
 tuned_em = build_direct_emulator(best_layer_hps, signal_train, par_train)
-em_loss, em_loss_val = train_emulator(tuned_em, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr, em_lr_factor,
-                                      em_lr_patience, em_lr_min_delta, em_min_lr, es_min_delta, es_patience)
+em_loss, em_loss_val = train_emulator(tuned_em, signal_train, signal_val, par_train, par_val, EPOCHS, em_lr,
+                                      em_lr_factor, em_lr_patience, em_lr_min_delta, em_min_lr, es_min_delta,
+                                      es_patience)
 
 # Can plot the  training curves like this
 # import matplotlib.pyplot as plt
