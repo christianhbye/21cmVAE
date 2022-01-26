f3d086a378905cfffa157cc2c2c75c6266b5057b
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-01-25 20:41:23 -0800

    import modules in init

--- VeryAccurateEmulator/__init__.py
+++ VeryAccurateEmulator/__init__.py
@@ -1,2 +1,8 @@
 __version__ = "2.0.0"
 __author__ = 'Christian Hellum Bye'
+
+from . import build_models
+from . import training_tools
+from . import emulator
+from . import hyperparameter_tuner
+from . import preprocess
