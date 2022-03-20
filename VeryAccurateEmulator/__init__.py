b603c4a461adb5002c0b9d8828a54a3311236cb9
Christian Hellum Bye <chbye@berkeley.edu>
2022-03-20 00:24:38 -0700

    blacken

--- VeryAccurateEmulator/__init__.py
+++ VeryAccurateEmulator/__init__.py
@@ -1,15 +1,18 @@
 __version__ = "3.0.0"
-__author__ = 'Christian Hellum Bye'
+__author__ = "Christian Hellum Bye"
 
 from . import emulator
 from . import preprocess
 
 from pathlib import Path
-HERE = __file__[:-len("__init__.py")]
-if not Path(HERE+"dataset_21cmVAE.h5").exists():
+
+HERE = __file__[: -len("__init__.py")]
+if not Path(HERE + "dataset_21cmVAE.h5").exists():
     import requests
+
     print("Downloading dataset.")
     r = requests.get(
         "https://zenodo.org/record/5084114/files/dataset_21cmVAE.h5?download=1"
     )
-    with open(HERE+"dataset_21cmVAE.h5", "wb") as f: f.write(r.content)
+    with open(HERE + "dataset_21cmVAE.h5", "wb") as f:
+        f.write(r.content)
