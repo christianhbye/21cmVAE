d6b9c9934f728508e53694dcdb4d05d4f642c85a
Christian Hellum Bye <chbye@berkeley.edu>
2022-03-20 00:22:05 -0700

    add some tests

--- tests/test_emulator.py
+++ tests/test_emulator.py
@@ -1,4 +1,12 @@
-from VeryAccurateEmulator import emulator
+import h5py
+import numpy as np
+import tensorflow as tf
+from VeryAccurateEmulator import emulator, __path__
+import VeryAccurateEmulator.preprocess as pp
+
+FILE = __path__[0] + "/dataset_21cmVAE.h5"
+with h5py.File(FILE, "r") as hf:
+    signal_train = hf["signal_train"][:]
 
 def test_gen_model():
     in_dim = 7
@@ -13,3 +21,25 @@ def test_gen_model():
         else:
             shape = layer.output_shape[-1]
         assert shape == all_dims[i]
+
+def test_relative_mse_loss():
+    loss_fcn = emulator.relative_mse_loss(signal_train)
+    y_true = pp.preproc(signal_train[42], signal_train)
+    amplitude = np.max(np.abs(signal_train[42]))
+    amplitude_proc = pp.preproc(amplitude, signal_train)
+    y_pred = signal_train[1203]
+    mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+    rel_mse = mse / amplitude_proc**2
+    assert np.isclose(rel_mse, loss_fcn(y_true, y_pred))
+
+def test_z_nu():
+    z = 30
+    nu = emulator.redshift2freq(z)
+    assert np.isclose(z, emulator.freq2redshift(nu))
+
+def test_error():
+    z = np.linspace(5, 50, 451)
+    nu = emulator.redshift2freq(z)
+    assert np.allclose(
+        error(signal_train, signal_train), np.zeros(len(signal_train))
+    )
