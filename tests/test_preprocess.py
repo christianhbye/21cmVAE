c78928835323df681e28c7e51b44053db20cd620
Christian Hellum Bye <chbye@berkeley.edu>
2022-03-19 23:54:19 -0700

    fix syntax and blacken

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,6 +1,5 @@
 import h5py
 import numpy as np
-import pytest
 from VeryAccurateEmulator import __path__
 import VeryAccurateEmulator.preprocess as pp
 
@@ -8,6 +7,7 @@ FILE = __path__[0] + "/dataset_21cmVAE.h5"
 with h5py.File(FILE, "r") as hf:
     signal_train = hf["signal_train"][:]
     par_train = hf["par_train"][:]
+
 
 def test_proc():
     proc_signal = pp.preproc(signal_train, signal_train)
@@ -18,9 +18,10 @@ def test_proc():
     unproc = pp.unpreproc(proc_signal, signal_train)
     assert np.allclose(unproc, signal_train)
 
-def test_par_transform()
+
+def test_par_transform():
     transformed = pp.par_transform(par_train, par_train)
     max_par = transformed.max(axis=0)
     min_par = transformed.min(axis=0)
     assert np.allclose(max_par, np.ones_like(max_par))
-    assert np.allcose(min_par, -1*np.ones_like(min_par))
+    assert np.allcose(min_par, -1 * np.ones_like(min_par))
